c0384e49737b647cca02f43c8bf5e5daf142e8a2
Nikita <buguev.nikita@gmail.com>
2021-03-12 15:52:02 +0500

    Architecture improvements

--- main.py
+++ main.py
@@ -1,39 +1,38 @@
-import requests
-import json
 from jsonrpc import JSONRPCResponseManager, dispatcher
-from flask import Flask
-from flask import render_template
-from flask import url_for
-from flask import request
+from flask import Flask, request, Blueprint
 
-app = Flask(__name__)
+# from . import db
+main = Blueprint('main', __name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@main.route('/profile')
+def profile():
+    return 'Profile'
+
+
+@main.route('/', methods=['GET', 'POST'])
 def index():
     return 'Help me'
+
+
+@main.route('/api/health', methods=['GET', 'POST'])
+def Health():
+    response = Flask.response_class(
+        status=200
+    )
+    return response
+
 
 @dispatcher.add_method
 def echo(input):
     return input
 
 
-@app.route('/api/health', methods=['GET', 'POST'])
-def Health():
-    response = app.response_class(
-        status=200
-    )
-    return response
-
-
-@app.route('/api/v1/jsonrpc', methods=['GET', 'POST'])
+@main.route('/api/v1/jsonrpc', methods=['GET', 'POST'])
 def application():
     response = JSONRPCResponseManager.handle(
         request.data, dispatcher)
-    return app.response_class(
+    return Flask.response_class(
         response=response.json,
         status=200
     )
-
-if __name__ == '__main__':
-    app.run(debug=True, port=8000)
