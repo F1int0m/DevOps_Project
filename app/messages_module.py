4cc85fe1c306b15dd991632975d54ae45a2ba509
F1int0m <buguev.nikita@gmail.com>
2021-04-15 21:18:12 +0500

    added email module

--- app/messages_module.py
+++ app/messages_module.py
@@ -1,2 +1,23 @@
-def Send_email(str):
-    print(str)
+import smtplib, ssl, os
+from email.message import EmailMessage
+from dotenv import load_dotenv
+
+load_dotenv()
+
+sender_email = "devops.send@gmail.com"
+password = os.getenv('password')
+
+
+def send_email(receiver_email="buguev.nikita@gmail.com", subject='test', text=' Hi! I do smth with gmail'):
+    msg = EmailMessage()
+    msg["Subject"] = subject
+    msg["From"] = sender_email
+    msg["To"] = receiver_email
+    msg.set_content(text)
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(
+            sender_email, receiver_email, msg.as_string()
+        )
