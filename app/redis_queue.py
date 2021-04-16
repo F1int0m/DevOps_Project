67e5bc42883b9b224f83152f8742aad2b67767c3
Nikita <buguev.nikita@gmail.com>
2021-04-16 22:52:04 +0500

    Edit dockerfile

--- app/redis_queue.py
+++ app/redis_queue.py
@@ -1,7 +1,7 @@
 from redis import Redis
 from rq import Queue
-from .messages_module import send_email
+from app.messages_module import send_email
 
 queue = Queue('abc', connection=Redis())
 
-# queue.enqueue(Send_email, 'content') #os.fork не работает на винде(
+queue.enqueue(send_email) #os.fork не работает на винде(
