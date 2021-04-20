bcbf5a84b3c20e26e77fb0ba3ffb05813bb14c5a
Nikita <buguev.nikita@gmail.com>
2021-04-20 17:26:11 +0500

    fix adding job to queue

--- app/redis_queue.py
+++ app/redis_queue.py
@@ -1,15 +1,16 @@
-from redis import Redis
-from rq import Queue
 from rq.job import Job
 from app.messages_module import send_email as smtp_send
+from redis import Redis
+from rq import Queue
 
-queue = Queue('abc', connection=Redis())
+redis = Redis()
+q = Queue(connection=redis)
 
 
 def send_email(receiver, subject, text):
     message_args = {'receiver_email': receiver, 'subject': subject, 'text': text}
-    job = Job.create(smtp_send, kwargs=message_args)
-    queue.enqueue(job)  # os.fork не работает на винде(
+    job = Job.create(smtp_send, kwargs=message_args, connection=redis)
+    q.enqueue_job(job)  # os.fork не работает на винде(
 
 
 send_email('buguev.nikita@gmail.com', 'abc', 'hahaha')
